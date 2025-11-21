02e830ef5874fc2891345472542f3ed982eaf621
Nordine Bittich <contact@bittich.be>
2025-11-21 10:47:43 +0100

    revert changes in the login script

--- scripts/project/generate-dashboard-login/run.sh
+++ scripts/project/generate-dashboard-login/run.sh
@@ -1,91 +1,78 @@
 #!/usr/bin/env ruby
 require 'bundler/inline'
-require 'yaml'
 require 'securerandom'
 require 'date'
+require 'bcrypt'
 
 $stdout.sync = true
-print "installing dependencies..."
-gemfile do
-  source 'https://rubygems.org'
-  gem 'bcrypt'
-end
-puts "done"
 
-salt = ""
-begin
-  File.open("/project/docker-compose.override.yml") do |file|
-    config = YAML.load(file)
-    salt = config.dig("services", "login", "environment", "MU_APPLICATION_SALT")
-    throw "invalid salt" unless salt
-   end
-rescue
-  puts "Tried to locate login salt in docker-compose.override.yml, but couldn't find it."
-  puts "Please make sure a salt is configured:"
-  puts <<EOF
-  login:
-    restart: "no"
-    environment:
-      MU_APPLICATION_SALT: "#{SecureRandom.hex}"
-EOF
-end
+puts
+puts "This script will generate a new user and account."
+puts "Enter the required information. Just hit enter to accept the default value."
+puts
 
-puts "this script will generate a new user account"
-print "username: "
+print "Person full name: "
+STDOUT.flush
+person_name = STDIN.gets.chomp
+
+print "Account name: "
 STDOUT.flush
 account_name = STDIN.gets.chomp
 
-print "password: "
+print "Password: "
 STDOUT.flush
 password = STDIN.gets.chomp
+
+default_salt = ""
+print "Application salt (default: none): "
+STDOUT.flush
+application_salt = STDIN.gets.chomp
+application_salt = default_salt if application_salt.empty?
+
+default_domain = "http://ext.data.gift"
+print "Resource domain (default: #{default_domain}): "
+STDOUT.flush
+domain = STDIN.gets.chomp
+domain = default_domain if domain.empty?
 
 account_salt = SecureRandom.hex
 person_uuid = SecureRandom.uuid
 account_uuid = SecureRandom.uuid
-domain = "data.gift"
-hashed_password = BCrypt::Password.create(password + salt + account_salt)
+hashed_password = BCrypt::Password.create(password + application_salt + account_salt)
 now = DateTime.now.xmlschema
+
 query = %(
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX people: <http://#{domain}/people/>
-PREFIX accounts: <http://#{domain}/accounts/>
+PREFIX people: <#{domain}/people/>
+PREFIX accounts: <#{domain}/accounts/>
 PREFIX dcterms: <http://purl.org/dc/terms/>
 PREFIX mu:      <http://mu.semte.ch/vocabularies/core/>
-PREFIX ext:      <http://mu.semte.ch/vocabularies/ext/>
 PREFIX account: <http://mu.semte.ch/vocabularies/account/>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
 INSERT DATA {
-   GRAPH <http://mu.semte.ch/application> {
+   GRAPH <http://mu.semte.ch/graphs/users> {
      people:#{person_uuid} a foaf:Person ;
-                   foaf:name "#{account_name}";
-                   foaf:account accounts:#{account_uuid};
-                   mu:uuid "#{person_uuid}";
-                   dcterms:created "#{now}"^^xsd:datetime;
-                   dcterms:modified "#{now}"^^xsd:datetime.
-     accounts:#{account_uuid} a foaf:OnlineAccount;
-                                                   foaf:accountName "#{account_name}";
-                                                   mu:uuid "#{account_uuid}";
-                                                   account:password """#{hashed_password}""";
-                                                   account:salt "#{account_salt}";
-                                                   account:status <http://mu.semte.ch/vocabularies/account/status/active>;
-                                                   dcterms:created "#{now}"^^xsd:datetime;
-                                                   ext:sessionRole "dashboard-user";
-                                                   dcterms:modified "#{now}"^^xsd:datetime.
-}}
+                   foaf:name "#{person_name}" ;
+                   foaf:account accounts:#{account_uuid} ;
+                   mu:uuid "#{person_uuid}" ;
+                   dcterms:created "#{now}"^^xsd:dateTime ;
+                   dcterms:modified "#{now}"^^xsd:dateTime .
+     accounts:#{account_uuid} a foaf:OnlineAccount ;
+                   foaf:accountName "#{account_name}" ;
+                   mu:uuid "#{account_uuid}" ;
+                   account:password """#{hashed_password}""" ;
+                   account:salt "#{account_salt}" ;
+                   account:status <http://mu.semte.ch/vocabularies/account/status/active> ;
+                   dcterms:created "#{now}"^^xsd:dateTime ;
+                   dcterms:modified "#{now}"^^xsd:dateTime .
+    }
+}
 )
 
-# Get current working directory
-project_root = Dir.pwd
-
-# Ensure the migrations directory exists
-migration_dir = File.join(project_root, "..", "..", "..", "config", "migrations", "local")
-Dir.mkdir(migration_dir) unless Dir.exist?(migration_dir)
-
-# Build filename
-timestamp = DateTime.now.strftime("%Y%m%d%H%M%L")
-filename = "#{timestamp}-create-user-#{account_name.gsub(" ", "-")}.sparql"
-
-# Write file
-File.write(File.join(migration_dir, filename), query)
-
-puts "generated migration ./config/migrations/local/#{filename} for #{account_name.inspect} : #{password.inspect}"
+timestamp = DateTime.now.strftime("%Y%m%d%H%M%S")
+filename = "#{timestamp}-create-user-#{account_name.gsub(/\s|\./, "-")}.sparql"
+puts
+puts "Copy the following contents to ./config/migrations/#{filename}"
+puts
+puts query
